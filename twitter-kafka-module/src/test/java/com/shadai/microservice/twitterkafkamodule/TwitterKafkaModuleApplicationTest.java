af4e66bff747ca2eaf98eb705324a9c2a1745cad
Shadai Lopez <shadai.lopez.garcia@gmail.com>
2024-06-06 19:13:45 -0600

    Remove unnecesary files
    Signed-off-by: Shadai Lopez <shadai.lopez.garcia@gmail.com>

--- twitter-kafka-module/src/test/java/com/shadai/microservice/twitterkafkamodule/TwitterKafkaModuleApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package com.shadai.microservice.twitterkafkamodule;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class TwitterKafkaModuleApplicationTest {
-
-}
